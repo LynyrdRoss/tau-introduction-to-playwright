b30472416e0bc18b24e214de675806dbc2595a07
LynyrdRoss <lyrhen17@gmail.com>
2023-12-05 09:45:05 +0800

    Remove only flag

--- tests/myExample.spec.ts
+++ tests/myExample.spec.ts
@@ -15,7 +15,7 @@ test('click at Get Started', async ({ page }) => {
   await expect(page).toHaveURL(/.*intro/)
 })
 
-test.only('click at Java language', async ({ page }) => {
+test('click at Java language', async ({ page }) => {
   const javaDescription = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`
 
   await page.getByRole('link', { name: 'Get started' }).click()
